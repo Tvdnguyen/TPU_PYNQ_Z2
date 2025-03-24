d220997983d5f0268e2cb3722acb3af71ac6fee5
Nguyen Trinh <118814159+Tvdnguyen@users.noreply.github.com>
2025-03-24 11:45:17 -0400

    Update sytolic_input_setup.v

--- src/sytolic_input_setup.v
+++ src/sytolic_input_setup.v
@@ -1,5 +1,9 @@
-//
-// systolic_input_setup.v
+//-----------------------------------------------------------------------------
+// Module: systolic_input_setup
+// Author: Nguyen Trinh
+// Created: jan 10, 2025
+// Last Updated: March 23, 2025
+
 // 
 // Using shift registers to skew the word so that the timing is correct for
 // futhur computation.
@@ -13,8 +17,6 @@
 //  |  |  |  |  |  |  buf_q7
 //  |  |  |  |  |  |  |   buf_q8
 //  |  |  |  |  |  |  |   |  buf_q9
-//  |  |  |  |  |  |  |   |  |  buf_q10
-//  |  |  |  |  |  |  |   |  |  |  
 `ifndef _SYSTOLIC_INPUT_SETUP_V
 `define _SYSTOLIC_INPUT_SETUP_V
 
@@ -54,15 +56,19 @@ module systolic_input_setup (
       buf_q5 <= 'd0;
       buf_q6 <= 'd0;
       buf_q7 <= 'd0;
+      buf_q8 <= 'd0;
+      buf_q9 <= 'd0;
     end else begin
       if (en_i) begin
         buf_q1 <= word_i[`WORD_WIDTH-1:`DATA_WIDTH];
-        buf_q2 <= buf_q1[`DATA_WIDTH*7-1:`DATA_WIDTH];
-        buf_q3 <= buf_q2[`DATA_WIDTH*6-1:`DATA_WIDTH];
-        buf_q4 <= buf_q3[`DATA_WIDTH*5-1:`DATA_WIDTH];
-        buf_q5 <= buf_q4[`DATA_WIDTH*4-1:`DATA_WIDTH];
-        buf_q6 <= buf_q5[`DATA_WIDTH*3-1:`DATA_WIDTH];
-        buf_q7 <= buf_q6[`DATA_WIDTH*2-1:`DATA_WIDTH];
+        buf_q2 <= buf_q1[`DATA_WIDTH*9-1:`DATA_WIDTH];
+        buf_q3 <= buf_q2[`DATA_WIDTH*8-1:`DATA_WIDTH];
+        buf_q4 <= buf_q3[`DATA_WIDTH*7-1:`DATA_WIDTH];
+        buf_q5 <= buf_q4[`DATA_WIDTH*6-1:`DATA_WIDTH];
+        buf_q6 <= buf_q5[`DATA_WIDTH*5-1:`DATA_WIDTH];
+        buf_q7 <= buf_q6[`DATA_WIDTH*4-1:`DATA_WIDTH];
+        buf_q8 <= buf_q7[`DATA_WIDTH*3-1:`DATA_WIDTH];
+        buf_q9 <= buf_q8[`DATA_WIDTH*2-1:`DATA_WIDTH];
       end
     end
   end
